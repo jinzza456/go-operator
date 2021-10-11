8c17a852fc3a8f24ad77feffc6430d0f3c16acae
jinzza456 <jinzza456@gmail.com>
2021-10-11 16:33:59 +0900

    if,if else

--- op.go
+++ op.go
@@ -17,10 +17,42 @@ func main() {
 
 	// fmt.Printf("첫번째 수 : %v\n두번째 수 : %v\n", c, d)
 
-	a := 4
+	// a := 4
 
-	fmt.Println(a &^ a)
-	fmt.Println(a << 1)
-	fmt.Println(a >> 1)
+	// fmt.Println(a &^ a)
+	// fmt.Println(a << 1)
+	// fmt.Println(a >> 1)
+
+	// var a bool
+	// a = 3 < 4 && 2 < 5
+
+	// fmt.Println(a)
+
+	// if 3 > 4 {
+	// 	fmt.Println("참")
+	// }
+	// fmt.Println("거짓")
+
+	// a := 3
+	// if a == 3 {
+	// 	fmt.Println ("a 는 3")
+	// }
+	// fmt.Println("a의 값은 ",a)
+
+	// a := 4
+	// if a == 3 {
+	// 	fmt.Println("a 는 3이다.")
+	// } else {
+	// 	fmt.Println("a는 3이 아니다.")
+	// }
+
+	a := 4
+	if a == 3 {
+		fmt.Println("a 는 3이다.")
+	} else if a == 4 {
+		fmt.Println("a는 4이다.")
+	} else {
+		fmt.Println("a는 3도 아니고 4도 아니다.")
+	}
 
 }
