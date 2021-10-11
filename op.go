22ed3ee7ffcf5854b36b085e22e07e6034648be5
jinzza456 <jinzza456@gmail.com>
2021-10-11 15:34:18 +0900

    %operator

--- op.go
+++ op.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	a := 4
-	b := 2
+	// a := 4
+	// b := 2
 
-	fmt.Printf("a&b = %v\n", a&b)
-	fmt.Printf("a|b = %v\n", a|b)
-	fmt.Println("a^b =", a^b)
+	// fmt.Printf("a&b = %v\n", a&b)
+	// fmt.Printf("a|b = %v\n", a|b)
+	// fmt.Println("a^b =", a^b)
+
+	a := 21
+	c := a % 10
+	a = a / 10
+	d := a % 10
+
+	fmt.Printf("첫번째 수 : %v\n두번째 수 : %v\n", c, d)
 }
