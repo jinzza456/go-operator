90efd48d5fc02739890077137b9de5a66e45a23d
jinzza456 <jinzza456@gmail.com>
2021-10-11 15:41:19 +0900

    &,>>,<<

--- op.go
+++ op.go
@@ -10,10 +10,17 @@ func main() {
 	// fmt.Printf("a|b = %v\n", a|b)
 	// fmt.Println("a^b =", a^b)
 
-	a := 21
-	c := a % 10
-	a = a / 10
-	d := a % 10
+	// a := 21
+	// c := a % 10
+	// a = a / 10
+	// d := a % 10
 
-	fmt.Printf("첫번째 수 : %v\n두번째 수 : %v\n", c, d)
+	// fmt.Printf("첫번째 수 : %v\n두번째 수 : %v\n", c, d)
+
+	a := 4
+
+	fmt.Println(a &^ a)
+	fmt.Println(a << 1)
+	fmt.Println(a >> 1)
+
 }
